66c8f330d31f4926671c259a0ee3219eb194daa6
TMGan <gangadeentim@gmail.com>
2025-07-29 00:09:20 -0400

    Initial commit

---
+++ tim-Assignment8/src/main/java/com/coderscampus/assignment/Assignment8Application.java
@@ -0,0 +1,11 @@
+package com.coderscampus.assignment;
+
+public class Assignment8Application {
+
+	public static void main(String[] args) {
+	Assignment8 assignment = new Assignment8();
+	assignment.processNumbers();
+
+	}
+
+}
